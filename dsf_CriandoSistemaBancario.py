05e20339dbab3f884e785a6dad445425e3838bc7
Gabriel Louzada <gabriellouzada@mail.com>
2023-07-08 18:33:02 -0300

    updated project Criando Sistema Bancario

--- dsf_CriandoSistemaBancario.py
+++ dsf_CriandoSistemaBancario.py
@@ -4,66 +4,71 @@ Created on Wed Jul  5 23:40:37 2023
 
 @author: Gabriel Louzada
 """
+import dsf_FuncSistemaBancario as fsb
 
-saldo = 2000
-limite_qtdsaque = 3
-limite_valsaque = 500
+usuarios = {}
+contas = {}
 extrato = ''
 
 print(" Banco DIO ".center(23, '='))
-print("")
-print("Olá, usuário! Seja bem-vindo(a) ao Banco DIO.")
 
 while True:
     print(f'''
-Seu saldo atual é de R${saldo:.2f}.
+Para utilizar o nosso sistema, por favor, selecione uma das opções abaixo:
+
+=========================
+[U] - Cadastrar usuário
+[C] - Cadastrar conta
+[L] - Logar na conta
+=========================
+''')
+
+    opcao = input("Digite a opção desejada: ").upper()
+    if opcao == 'U':
+        try:
+            fsb.novoUsuario(usuarios)
+        except:
+            continue
+    elif opcao == 'C':
+        try:
+            fsb.novaConta(usuarios, contas)
+        except:
+            print("Este CPF não existe no nosso banco de dados. Por favor, cadastre o usuário.")
+            continue
+    elif opcao == 'L':
+        try:
+            dados = fsb.login(usuarios, contas)
+            while True:
+                print(f'''
+Seu saldo atual é de R${dados[0]:.2f}.
 Por favor, digite o número da operação que você deseja realizar.
 
-====================
+=========================
 [1] - Depositar
 [2] - Sacar
 [3] - Consultar extrato
-====================
-        ''')
-    opcao = int(input("Digite a opção desejada: "))
-    if opcao == 1:
-        print("Qual valor você deseja depositar?")
-        deposito = int(input('Digite o valor em números: '))
-        if deposito > 0:
-            saldo += deposito
-            extrato += f"Depósito de R${deposito:.2f} \n"
-            print(f"O valor de R${deposito:.2f} foi depositado em sua conta.")
-        else:
-            print("Infelizmente o valor digitado não pôde ser computado.")
-            continue
-    elif opcao == 2:
-        if limite_qtdsaque > 0:
-            print(f'''
-Você ainda tem {limite_qtdsaque} saque(s) disponível(s) hoje.
-Lembre-se que o limite por saque é de R${limite_valsaque:.2f}!
-Por favor, digite quanto deseja sacar.
-              ''')
-            saque = int(input('Digite o valor em números: '))
-            if 0 < saque < saldo:
-                if saque <= limite_valsaque:
-                    saldo -= saque
-                    extrato += f"Saque de R${saque:.2f} \n"
-                    limite_qtdsaque -= 1
-                    print(f"O valor de R${saque:.2f} foi sacado da sua conta.")
-                else:
-                    print("O valor digitado é maior que o limite permitido.")
+=========================
+                ''')
+                opera = int(input("Digite a opção desejada: "))
+                if opera == 1:
+                    try:
+                        dados, extrato = fsb.depositar(dados, extrato)
+                    except:
+                        continue
+                elif opera == 2:
+                    try:
+                        dados, extrato = fsb.sacar(Conta=dados, listaExtrato=extrato)
+                    except:
+                        continue
+                elif opera == 3:
+                    print("")
+                    print(extrato)
                     continue
-            elif 0 < saldo < saque:
-                print("O valor digitado é maior que o seu saldo.")
-                continue
-            else:
-                print("Infelizmente o valor digitado não pôde ser computado.")
-                continue
-        else:
-            print("Você atingiu o limite de saques para hoje.")
+                else:
+                    print("Você foi deslogado do sistema.")        
+                    break
+        except:
             continue
-    elif opcao == 3:
-        print(extrato)
-        continue
-    else:        
+    else:
+        print("Obrigado por usar o Banco DIO. Até mais!")
         break
